93bac6a27ecd5c048a6705a415c638179093407b
Nolan Hewitt <nolanhewitt@aol.com>
2020-06-08 01:41:43 -0500

    test

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const passport = require("passport");
 
-const users = require("./routes/api/users");
-const comments = require("./routes/api/comments");
-require('dotenv').config();
 
 const app = express();
 
@@ -19,30 +14,6 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// DB Config
-const db = require("./config/keys").mongoURI;
-
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport config
-require("./config/passport")(passport);
-
-// Routes
-app.use("/api/users", users);
-app.use("/api/comments", comments);
-app.use("*", (req, res) =>
- res.sendFile(path.join(__dirname, "../client/build/index.html"))
-);
 
 
 const port = process.env.PORT || 5000;
